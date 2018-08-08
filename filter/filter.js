65a55b1bd76a7459bbc61d11ab4d5b6665972a15
IamGiel <lfagel84@gmail.com>
2018-08-08 10:30:17 -0400

    custom filter in angularjs

--- filter/filter.js
+++ filter/filter.js
@@ -1,22 +1,36 @@
+///<reference path=angular.js>
+
 var app = angular
     .module('myModule', [])
+    .filter('gender', function () {
+        return function (gender) {
+            switch (gender) {
+                case 1:
+                    return 'Male';
+                case 2:
+                    return 'Female';
+                case 3:
+                    return 'Not Disclosed';
+            }
+        }
+    })
     .controller('myController', function ($scope) {
         var Employees = [{
                 name: 'John',
                 dateOfBirth: new Date('November 13, 1990'),
-                gender: 'Male',
+                gender: 1,
                 salary: 60000
             },
             {
                 name: 'Mike',
                 dateOfBirth: new Date('September 23, 1990'),
-                gender: 'Male',
+                gender: 1,
                 salary: 60000
             },
             {
                 name: 'Ryu',
                 dateOfBirth: new Date('December 12, 1990'),
-                gender: 'Male',
+                gender: 2,
                 salary: 60000
             }
         ];
